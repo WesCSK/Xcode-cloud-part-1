eaf98f96c7a065505bd20a18bc721795be41cd82
Swee Kwang <sweekwang94@gmail.com>
2023-04-26 22:57:32 +0800

    Initial Commit

---
+++ VoiceGuide/VoiceGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceGuideApp.swift
+//  VoiceGuide
+//
+//  Created by Swee Kwang Chua on 26/4/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
